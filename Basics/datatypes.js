f467258cf4e99a263da2a8ecc9fbe6cf65593a36
shaikmeharunnisa123 <147305622+shaikmeharunnisa123@users.noreply.github.com>
2024-07-29 12:11:34 +0000

    datatypes_variables

---
+++ Basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+//alert( 3 + 3) //we are using nodejs not browser
+
+console.log(3+3); //code readability should be high
+
+console.log("Mehar");
+
+let name="Mehar" //String
+let age=19;  //number
+let isLoggedIn=false; //boolean
+let state;
+
+//number => 2 to power 53
+//bigint => 
+// string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol =>unique
+
+
+// object
+
+console.log(typeof(age));
+
+console.log(typeof undefined);
+console.log(typeof null);
---
+++ Basics/variables.js
@@ -0,0 +1,20 @@
+const accountId=144553;
+let accountEmal="mehar@google.com";
+var accountPassword="12345";
+accountCity="Hyderabad";
+let accountState;
+
+//accountId=2 //not allowed
+
+
+accountEmail="m23@google.com";
+accountPassword="23456";
+accountCity="Vijayawada";
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope 
+*/
